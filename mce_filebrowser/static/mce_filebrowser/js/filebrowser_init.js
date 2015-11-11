8ee0e988484a5a66ca397982171a1c8fadc40927
Jenna Howe <jenna.howe@live.co.uk>
2015-11-11 16:37:53 +0000

    Try hardcoding domain to see if that fixes the problem--temporary

--- mce_filebrowser/static/mce_filebrowser/js/filebrowser_init.js
+++ mce_filebrowser/static/mce_filebrowser/js/filebrowser_init.js
@@ -1,5 +1,7 @@
 function mce_filebrowser(field_name, url, type, win) {
-    document.domain = SETTINGS.get("DOCUMENT_DOMAIN");//this is to allow cross subdomain loading of popups
+    // document.domain = SETTINGS.get("DOCUMENT_DOMAIN");//this is to allow cross subdomain loading of popups
+    document.domain = "http://172.20.30.101:8000";//this is to allow cross subdomain loading of popups
+    
     tinyMCE.activeEditor.windowManager.open({
         url: "/mce_filebrowser/" + type + "/",
         title: 'Insert media',
