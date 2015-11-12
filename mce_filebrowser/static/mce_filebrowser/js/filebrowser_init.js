db3ff4ca9e27d47270dd8a4377d57ce757a0ef30
Jenna Howe <jenna.howe@live.co.uk>
2015-11-12 11:13:40 +0000

    Fix document.domain problem

--- mce_filebrowser/static/mce_filebrowser/js/filebrowser_init.js
+++ mce_filebrowser/static/mce_filebrowser/js/filebrowser_init.js
@@ -1,6 +1,5 @@
 function mce_filebrowser(field_name, url, type, win) {
     // document.domain = SETTINGS.get("DOCUMENT_DOMAIN");//this is to allow cross subdomain loading of popups
-    document.domain = "fullfact.org";//this is to allow cross subdomain loading of popups
     
     tinyMCE.activeEditor.windowManager.open({
         url: "/mce_filebrowser/" + type + "/",
